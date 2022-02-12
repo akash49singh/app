71ed90d7fbbcca206c27ef7a08c1c306a2fbb412
Akash Singh <65590662+akash49singh@users.noreply.github.com>
2022-02-12 19:16:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 #import joblib # Serialization
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
 import plotly.express as px
