a9d6eeaea5148674ae99ac6845f174e015223b21
Akash Singh <65590662+akash49singh@users.noreply.github.com>
2022-02-18 20:49:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,74 +1,61 @@
-import streamlit as st # web development
-import numpy as np # np mean, np random 
-import pandas as pd # read csv, df manipulation
-import time # to simulate a real time data, time loop 
-import plotly.express as px # interactive charts 
+import streamlit as st
+import joblib # Serialization
+import matplotlib.pyplot as plt
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
+ed = pd.read_csv("covid_india.csv")
+#df = df.rename(columns={'Date':'date','State/UnionTerritory':'state'})
+#df = df.rename(columns={'Cured':'Cured','Deaths':'death','Confirmed':'confirm'})
+#df = df['Active_Cases']=df['Confirmed']-(df['Cured']+df['Deaths'])
+ed = ed[{'Date','State/UnionTerritory','Cured','Deaths','Confirmed'}]
+ed = ed[ed['Date']=='2021-08-11']
 
-# read csv from a github repo
-df = pd.read_csv("covid_india.csv")
+ed = ed.sort_values(by='Confirmed', ascending=False)
+ed = ed.head(5)
 
+df = ed.groupby('State/UnionTerritory')['Confirmed','Deaths','Cured'].max().reset_index()
 
-st.set_page_config(
-    page_title = 'Real-Time Dashboard',
-    page_icon = '✅',
-    layout = 'wide'
-)
+st.title("Covid-19 Dashboard For India")
+st.markdown('The dashboard will visualize the Covid-19 Situation in India')
+#st.markdown('Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus. Most people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special treatment.')
+st.sidebar.title("Visualization Selector")
+st.sidebar.markdown("Select the Charts/Plots accordingly:")
 
-# dashboard title
+select = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='1')
+if not st.sidebar.checkbox("Hide", False, key='1'):
+    if select == 'Pie chart':
+                        st.title("Selected top 5 States")
+                        fig = px.pie(df, values=df["Confirmed"][:5], names=df["State/UnionTerritory"][:5], title="Total Confirmed Cases")
+                        st.plotly_chart(fig)
 
-st.title("Real-Time / Live Data Science Dashboard")
+    if select=='Bar plot':
+                        st.title("Selected Top 5 States")
+                        fig = go.Figure(data=[
+                        go.Bar(name='Confirmed', x=df['State/UnionTerritory'][:5], y=df['Confirmed'][:5]),
+                        go.Bar(name='Cured', x=df['State/UnionTerritory'][:5], y=df['Cured'][:5]),
+                        go.Bar(name='Deaths', x=df['State/UnionTerritory'][:5], y=df['Deaths'][:5])])
+                        st.plotly_chart(fig)
 
-# top-level filters 
+df2 = pd.read_csv('covid_india.csv')
+df2['Date'] =  df2['Date'].astype('datetime64[ns]')
+select1 = st.sidebar.selectbox('Select', ['Confirmed', 'Cured', 'Deaths'], key='2')
+if not st.sidebar.checkbox("Hide", False, key='3'):
+    if select1 == 'Confirmed':
+        fig = px.line(df2, x="Date", y="Confirmed")
+        st.plotly_chart(fig)
+    elif select1 == 'Cured':
+        fig = px.line(df2, x="Date", y="Cured")
+        st.plotly_chart(fig)
 
-state_filter = st.selectbox("Select the state", pd.unique(df['State/UnionTerritory']))
-
-
-# creating a single-element container.
-placeholder = st.empty()
-
-# dataframe filter 
-
-df = df[df['State/UnionTerritory']==state_filter]
-
-# near real-time / live feed simulation 
-
-for seconds in range(200):
-#while True: 
-    
-    df['Confirmed'] = df['Confirmed'] * np.random.choice(range(1,5))
-    df['Deaths'] = df['Deaths'] * np.random.choice(range(1,5))
-
-    # creating KPIs 
-    avg_age = np.sum(df['Confirmed']) 
-
-    count_married = int(df[(df["Cured"]=='Cured')]['Cured'].count() + np.random.choice(range(1,30)))
-    
-    balance = np.mean(df['Deaths'])
-
-    with placeholder.container():
-        # create three columns
-        kpi1, kpi2, kpi3 = st.columns(3)
-
-        # fill in those three columns with respective metrics or KPIs 
-        kpi1.metric(label="Age ⏳", value=(avg_age), delta= round(avg_age) - 10)
-        kpi2.metric(label="Married Count 💍", value= int(count_married), delta= - 10 + count_married)
-        kpi3.metric(label="A/C Balance ＄", value= f"$ {round(balance,2)} ", delta= - round(balance/count_married) * 100)
-
-        # create two columns for charts 
-
-        fig_col1, fig_col2 = st.columns(2)
-        with fig_col1:
-            st.markdown("### First Chart")
-            fig = px.density_heatmap(data_frame=df, y = 'Confirmed', x = 'State/UnionTerritory')
-            st.write(fig)
-        with fig_col2:
-            st.markdown("### Second Chart")
-            fig2 = px.histogram(data_frame = df, x = 'Confirmed')
-            st.write(fig2)
-        st.markdown("### Detailed Data View")
-        st.dataframe(df)
-        time.sleep(1)
-    #placeholder.empty()
-
-
+select2 = st.sidebar.selectbox('Select', ['Confirmed', 'Cured'], key='3')
+if not st.sidebar.checkbox("Hide", False, key='4'):
+    if select2 == 'Confirmed':
+        fig = px.area(df2, x="Date", y="Confirmed")
+        st.plotly_chart(fig)
+    elif select1 == 'Cured':
+        fig = px.area(df2, x="Date", y="Cured")
+        st.plotly_chart(fig)
