fbf064768529dcc10598265a2be4b660a04a11a1
Akash Singh <65590662+akash49singh@users.noreply.github.com>
2022-02-18 20:28:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ for seconds in range(200):
         fig_col1, fig_col2 = st.columns(2)
         with fig_col1:
             st.markdown("### First Chart")
-            fig = px.density_heatmap(data_frame=df, y = 'Confirmed', x = 'marital')
+            fig = px.density_heatmap(data_frame=df, y = 'Confirmed', x = 'State/UnionTerritory')
             st.write(fig)
         with fig_col2:
             st.markdown("### Second Chart")
