aaf5ee503b1e855a36279de2608712e2cd19373c
Akash Singh <65590662+akash49singh@users.noreply.github.com>
2022-02-18 20:23:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ placeholder = st.empty()
 
 # dataframe filter 
 
-df = df[df['State/UnionTerritory']==State/UnionTerritory_filter]
+df = df[df['State/UnionTerritory']==state_filter]
 
 # near real-time / live feed simulation 
 
